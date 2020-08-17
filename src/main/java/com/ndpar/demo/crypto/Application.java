49fc0a7c61024cc1d674c6303e19dbf524478fe1
Andrey Paramonov <github@ndpar.com>
2020-08-17 15:04:32 -0400

    Add BouncyCastle security provider

--- src/main/java/com/ndpar/demo/crypto/Application.java
+++ src/main/java/com/ndpar/demo/crypto/Application.java
@@ -1,10 +1,17 @@
 package com.ndpar.demo.crypto;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class Application {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
